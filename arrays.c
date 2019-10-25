c49afb9b9fbba36756126b34cc6c321977813581
Misha <michael.krasa@student.manchester.ac.uk>
2019-10-25 11:07:47 +0100

    ex3

--- arrays.c
+++ arrays.c
@@ -13,7 +13,7 @@ static void insert(struct person* people[], char *name, int age, int *nextinsert
 {
   /* put name and age into the next free place in the array parameter here */
   people[(*nextinsert)] = malloc(sizeof(person));
-  people[(*nextinsert)]->name = malloc(sizeof(char)*sizeof(name));
+  people[(*nextinsert)]->name = malloc(sizeof(char)*sizeof(names));
   strcpy(people[(*nextinsert)]->name, name);
   people[(*nextinsert)]->age = age;
   /* modify nextfreeplace here */
@@ -23,13 +23,13 @@ static void insert(struct person* people[], char *name, int age, int *nextinsert
 int main(int argc, char **argv)
 {
   int* testMalloc = malloc(15000000);
-  if(!testMalloc)
+  if(testMalloc == NULL)
     return 1;
 
   free(testMalloc);
 
   // Declaring people array yeet
-  struct person* people[HOW_MANY];
+  struct person *people[HOW_MANY];
   int nextinsert = 0;
   int i = 0;
   for (i=0;i < HOW_MANY; i++)
