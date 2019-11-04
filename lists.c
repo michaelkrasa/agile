0802f2c651ec393cd2d2ab65cfc60e1966c6eb5a
Misha <michael.krasa@student.manchester.ac.uk>
2019-11-04 13:44:21 +0000

    ex4

--- lists.c
+++ lists.c
@@ -1,29 +1,81 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include "arrays.h"
+#include "data.h"
 
-/* declare your struct for a person here */
-struct person {
+/* declare union for a person here */
+typedef struct person {
   char* name;
   int age;
   struct person* next;
-};
+} person;
 
-static void insert(person* head, char *name, int age)
+static person* insert_start(person* head, char *name, int age)
 {
-  Person* new = (Person*)malloc(sizeof(struct Person));
-  new->name = (Person*)malloc(sizeof(char)*sizeof(names));
+  struct person* new = (struct person*)malloc(sizeof(struct person));
+  new->name = malloc(sizeof(char)*strlen(name));
+  strcpy(new->name, name);
+  new->age = age;
+  new->next = head;
+  head = new;
+  return head;
+}
+
+
+static person* insert_end(struct person* head, char *name, int age)
+{
+  struct person* new = (struct person*)malloc(sizeof(struct person));
+  struct person* prev = head;
+
+  new->name = malloc(sizeof(char)*strlen(name));
   new->name = name;
   new->age = age;
 
-  new->next = head;
-  head = new;
-  /* modify nextfreeplace here */
-  
+  if (head == NULL)
+  {
+     head = new;
+     return head;
+  }
+
+  while (prev->next != NULL)
+    prev = prev->next;
+
+  prev->next = new;
+  return head;
 }
 
-int main(int argc, char **argv)
+/*static person* insert_sorted(struct person* head, char *name, int age)
+{
+  struct person* new = (struct person*)malloc(sizeof(struct person));
+  struct person* prev = head;
+
+  new->name = malloc(sizeof(char)*strlen(name));
+  new->name = name;
+  new->age = age;
+
+  if (head == NULL)
+  {
+     head = new;
+     return head;
+  }
+
+  while (prev->next != NULL)
+    prev = prev->next;
+
+  prev->next = new;
+  return head;
+}*/
+
+void printList(struct person *node)
+{
+  while (node != NULL)
+  {
+    printf("Name: %s, age: %d\n", node->name, node->age);
+    node = node->next;
+  }
+}
+
+int main(int argc, char* argv)
 {
   int* testMalloc = malloc(15000000);
   if(testMalloc == NULL)
@@ -31,12 +83,24 @@ int main(int argc, char **argv)
 
   free(testMalloc);
 
-  // Declaring people array yeet
-  person *head = NULL;
-  nextinsert = 0;
+  // Starting with an empty list
+  struct person* head = NULL;
+
   int i = 0;
   for (i=0;i < HOW_MANY; i++)
-    insert(head, names[i], ages[i]);
+  {
+    head = insert_start(head, names[i], ages[i]);
+  }
+  // Preserve the head before we iterate through it and print it
+  struct person* start = head;
+
+  printList(head);
+
+  for(i=0; i < HOW_MANY; i++)
+  {
+    free(people[i]->name);
+    free(people[i]);
+  }
 
   return 0;
 }
